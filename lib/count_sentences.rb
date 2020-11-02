09c548ed5c07167f629f5e9240071870d4afc2e1
Alicia Weenum <weenum.alicia@gmail.com>
2020-11-02 14:29:49 -0500

    commit tree af54671f3753f9b5966a6cb3525605b7ef3cacde

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if "Hi, my name is Sophie.".end_with?(".")
+      true 
+    else
+      false 
+    end
+    self.end_with?(".")
   end
 
   def question?
-
+    if "HI?".end_with?("?")
+      true 
+    else
+      false
+    end
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    if "HI!".end_with?("!")
+      true 
+    else
+      false
+    end
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.?!]+/).count
   end
 end
